724adb07821a68cf97d2070cad9f9c31a92fb59d
ryansldev <ryanlima1309@gmail.com>
2025-06-03 21:29:23 -0300

    add english translate

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-'use-client';
+'use client';
 
 import Image from "next/image";
 import { Card } from "../components/Card";
 import { CardList } from "../components/CardList";
+import { useEffect, useState } from "react";
+
+import dictionary from "./dictionary.json"
 
 export default function Home() {
+  const [language, setLanguage] = useState<"pt-br" | "en">("pt-br")
+
+  const texts = dictionary[language]
+
+  async function changeLanguage(newLanguage: "pt-br" | "en") {
+    setLanguage(newLanguage)
+    localStorage.setItem("lng", newLanguage)
+  }
+
+  useEffect(() => {
+    setLanguage(localStorage.getItem("lng") as "pt-br" | "en")
+  }, [])
+
   return (
     <main className="py-24">
       <div className="flex flex-col items-center justify-center">
@@ -15,30 +31,41 @@ export default function Home() {
           width={192}
           className="rounded-full mb-9"
         />
-        <h1 className="text-5xl font-extrabold tracking-[-0.04px] leading-[48px]">Ryan Lima</h1>
-        <h2 className="font-medium uppercase text-main-500 tracking-[-0.02px] leading-[44px]">FullStack Developer since 2018</h2>
+        <h1 className="text-5xl font-extrabold tracking-[-0.04px] leading-[48px]">
+          Ryan Lima
+        </h1>
+        <h2 className="font-medium uppercase text-main-500 tracking-[-0.02px] leading-[44px]">
+          FullStack Developer since 2018
+        </h2>
+        <button
+          type="button"
+          onClick={() => changeLanguage(language === "pt-br" ? "en" : "pt-br")}
+          className="text-neutral-400 bg-neutral-800 px-5 h-12 rounded-lg"
+        >
+          {texts?.changeTo} {language === "pt-br" ? "Inglês" : "Portuguese"}
+        </button>
       </div>
 
       <CardList>
         <Card
           title="Instagram"
-          description="É um ótimo local pra compartilhar do meu dia a dia"
+          description={texts?.instagramDescription}
           href="https://www.instagram.com/ryansldev/"
         />
         <Card
           title="LinkedIn"
-          description="É um ótimo local pra acompanhar publicações profissionais"
+          description={texts?.linkedInDescription}
           href="https://www.linkedin.com/in/ryansldev/"
         />
         <Card
           title="Projetos"
-          description="Dê uma olhada em meus projetos públicos"
+          description={texts?.projectsDescription}
           href="/projects"
         />
         <Card
           title="Currículo"
-          description="É um ótimo meio para ver detalhadamente as minhas habilidades"
-          href="/cv.pdf"
+          description={texts?.curriculumDescription}
+          href={language === "pt-br" ? "/cv.pdf" : "/cv-en.pdf"}
           download
         />
       </CardList>
